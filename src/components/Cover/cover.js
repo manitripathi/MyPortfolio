faa8c38816d58d9b830e1761e21f4d9be91c361e
manitripathi <manitripathi.023@gmail.com>
2021-11-18 01:09:29 +0530

    msg

--- src/components/Cover/cover.js
+++ src/components/Cover/cover.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './cover.css';
 import ManiResume from './Mani_Resume.pdf';
+import {Link} from "react-scroll";
 
-class Cover extends React.Component{
-    render(){
+function Cover(){
         return(
             <div className='headBackImg'>
                <div className='t1'>Hi, I am Mani Tripathi</div>
@@ -12,14 +12,15 @@ class Cover extends React.Component{
                <p className='t3'>Get ready to turn ideas into reality</p>
                <div className='flexBtn'>
                    <button className='btn btn1' id='btn1'>
-                   <a href='#Contact' className='btn11'>Hire Me</a>
+                   {/* <a href='#Contact' className='btn11'>Hire Me</a> */}
+                   <Link to='Contact' className='btn11' spy={true} smooth={true}>Hire Me</Link>
                    </button>
                    <button className='btn btn2' id='btn2'>
-                   <a href={ManiResume} className='btn11'>Get Resume</a>
+                   <a href={ManiResume} className='btn11'><span className='pdf'>Get Resume</span></a>
+                   
                    </button>
             </div>
             </div>
         );
-    }
 }
 export default Cover;
